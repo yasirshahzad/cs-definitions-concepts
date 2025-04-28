244e0c1cc7a262b9934531a627edb2c64a79138b
Muhammad Yasir <shahzadyasir77@gmail.com>
2025-04-28 14:54:57 +0500

    added pipeline

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,31 +1,11 @@
+const fs = require("fs");
 const http = require("http");
+const { pipeline } = require("stream/promises");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+const server = http.createServer(async (req, res) => {
+  const stream = fs.createReadStream("./bigfile.txt");
 
-  // OR
-
-  // res.setHeaders({ "Content-Type": "text/plain" });
-
-  res.end("Hello World\n");
+  await pipeline(stream, res);
 });
 
-server.on("request", (req, res) => {
-  console.log(`Request received: ${req.method} ${req.url}`);
-});
-
-server.on("close", () => {
-  console.log("Server closed");
-});
-
-server.on("error", (err) => {
-  console.error("Server error:", err);
-});
-
-server.on("connection", (socket) => {
-  console.log("New connection established");
-});
-
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000/");
-});
+server.listen(3000, () => console.log("Streaming server running"));
